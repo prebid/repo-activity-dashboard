22b7de3565a35f53b79e6eaedf5cf36baa95c1e5
agentmoose <phoenixtechnerd@gmail.com>
2025-09-23 13:40:22 -0600

    fix lambda access

--- src/lib/aws/clients.ts
+++ src/lib/aws/clients.ts
@@ -5,8 +5,8 @@ import { S3Client } from "@aws-sdk/client-s3";
 const config = {
   region: process.env.DYNAMODB_REGION || process.env.S3_REGION || process.env.AWS_REGION || 'us-east-2',
   credentials: {
-    accessKeyId: process.env.S3_ACCESS_KEY_ID || process.env.AWS_ACCESS_KEY_ID!,
-    secretAccessKey: process.env.S3_SECRET_ACCESS_KEY || process.env.AWS_SECRET_ACCESS_KEY!,
+    accessKeyId: process.env.S3_ACCESS_KEY_ID || process.env.AWS_ACCESS_KEY_ID || '',
+    secretAccessKey: process.env.S3_SECRET_ACCESS_KEY || process.env.AWS_SECRET_ACCESS_KEY || '',
   },
 };
 
